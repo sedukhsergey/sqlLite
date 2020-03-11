39046c0b4b186c2c97893b4f40e044173fb7df2a
sergey-sedykh <sedukhsergey@gmail.com>
2020-03-11 14:00:45 +0200

    Add sqlLite service

--- routes/articles.js
+++ routes/articles.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('../models/Article').Article;
 
 router.get('/', (req, res, next) => {
   if (req.query.id) {
     next();
     return;
   }
-  res.send('all articles')
+  Article.all((err, data) => {
+    if (err) res.send(err);
+    res.send(data);
+  })
 });
 
 router.get('/', (req, res, next) => {
   // req.query.id
-  res.send('One Article')
-})
-
+  res.send('One Article');
+});
 
 router.post('/', (req, res, next) => {
-  res.send('Post Ok')
+  if (req.query.id) {
+    console.log('req.body.title', req.body.title);
+  }
+  res.send('Post Ok');
 });
 
 module.exports = router;
