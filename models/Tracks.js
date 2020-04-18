65bc0a96a4c967210473e50f58144a1783e8dfd7
sergey-sedykh <sedukhsergey@gmail.com>
2020-04-18 18:12:35 +0300

    create tracks

--- models/Tracks.js
+++ models/Tracks.js
@@ -2,7 +2,16 @@ const db = require('./db');
 
 class Tracks {
   static all(cb) {
-
+    db.all('SELECT * FROM tracks', cb);
+  }
+  static find(id, cb) {
+    db.get('SELECT * FROM tracks WHERE id = ?', id, cb);
+  }
+  static create({ albumId, composer, milliseconds }, cb) {
+    if ( !albumId || !composer || !milliseconds) {
+      return cb(new Error('Please provide all params'))
+    }
+    const sql = 'INSERT INTO tracks(albumId, composer, milliseconds)'
   }
 }
 
